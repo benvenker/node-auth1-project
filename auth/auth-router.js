bcafa6af01f71e91c4a63fe05ac65aa81b748af3
Ben Venker <bvemails@gmail.com>
2020-04-30 11:41:10 -0500

    add login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -18,4 +18,21 @@ router.post("/register", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session.user = username;
+        res.status(200).json({ message: "Welcome! " });
+      } else {
+        res.status(401).json({ message: "Youo shall not pass!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "problem with the db", error: err });
+    });
+});
+
 module.exports = router;
